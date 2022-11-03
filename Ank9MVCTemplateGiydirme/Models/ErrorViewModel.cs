6e8d9a88eea8f24ec03d458047151e1bfd70554e
Onur Alp Aydın <o.a.aydin17@gmail.com>
2022-11-03 16:03:54 +0300

    Add project files.

---
+++ Ank9MVCTemplateGiydirme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ank9MVCTemplateGiydirme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
