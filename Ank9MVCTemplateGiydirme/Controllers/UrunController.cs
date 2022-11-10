fa1c09c53a6aa26c259de2d967f72a386c3e15e3
Onur Alp Aydın <o.a.aydin17@gmail.com>
2022-11-10 14:30:53 +0300

    Include Added

---
+++ Ank9MVCTemplateGiydirme/Controllers/UrunController.cs
@@ -0,0 +1,23 @@
+﻿using Ank9MVCTemplateGiydirme.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ank9MVCTemplateGiydirme.Controllers
+{
+    public class UrunController : Controller
+    {
+        private readonly RestaurantContext _db;
+
+        public UrunController(RestaurantContext db)
+        {
+           _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            var bununTipiNe = _db.Urunler.Include(x => x.Kategori).Include(x => x.UrunlerMalzemeler).ThenInclude(x => x.Malzeme).OrderByDescending(x => x.UrunId).ToList();
+
+            return View(bununTipiNe);
+        }
+    }
+}
